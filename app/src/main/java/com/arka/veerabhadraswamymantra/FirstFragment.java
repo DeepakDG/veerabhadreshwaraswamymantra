c245ebfc325ec0c6c66ce0e14f78856a89b6348a
DeepakWin10 <deepakganachari3@gmail.com>
2022-02-23 20:39:23 +0530

    first commit

---
+++ app/src/main/java/com/arka/veerabhadraswamymantra/FirstFragment.java
@@ -0,0 +1,37 @@
+package com.arka.veerabhadraswamymantra;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.arka.veerabhadraswamymantra.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends ListFragment {
+
+    String[] code_name = {"Froyo", "GingerBread", "HoneyComb", "IceCream Sandwich", "JellyBean", "Kitkat", "Lollipop", "Marshmallow", "Nougat", "Oreo"};
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, code_name);
+        setListAdapter(adapter);
+        return view;
+
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(getActivity().getBaseContext(), code_name[position], Toast.LENGTH_SHORT).show();
+    }
+}
