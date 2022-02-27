93d367ab8cc738cd48a898f3905daa52e9234a10
DeepakWin10 <deepakganachari3@gmail.com>
2022-02-27 12:04:30 +0530

    added dynamic pagerview

--- app/src/main/java/com/arka/veerabhadraswamymantra/PagerActivity.java
+++ app/src/main/java/com/arka/veerabhadraswamymantra/PagerActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.viewpager.widget.ViewPager;
 
@@ -38,18 +40,45 @@ public class PagerActivity extends Activity
         TextView textView1 = new TextView(this);
         textView1.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView1.getLayoutParams().width = 300;
+        textView1.getLayoutParams().height = 300;
         textView1.setText("programmatically created TextView1");
         textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
         textView1.setPadding(20, 20, 20, 20);// in pixels (left, top, right, bottom)
         addView(textView1);
 
+
+
         TextView textView2 = new TextView(this);
         textView2.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView2.getLayoutParams().width = 300;
+        textView2.getLayoutParams().height = 300;
         textView2.setText("programmatically created TextView2");
         textView2.setBackgroundColor(0xff660066); // hex color 0xAARRGGBB
         textView2.setPadding(20, 20, 20, 20);// in pixels (left, top, right, bottom)
         addView(textView2);
+
+        Button backButton=(Button)findViewById(R.id.btn_back);
+        Button nextButton=(Button)findViewById(R.id.btn_next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Next",Toast.LENGTH_SHORT).show();
+                pager.setCurrentItem (pager.getCurrentItem()+1, true);
+            }
+        });
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Back",Toast.LENGTH_SHORT).show();
+                pager.setCurrentItem (pager.getCurrentItem()-1, true);
+            }
+        });
     }
 
     //-----------------------------------------------------------------------------
